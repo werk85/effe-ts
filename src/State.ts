a8183a1f67a3e86af981b61b7edb5e7b38566c38
Malte Legenhausen <mlegenhausen@gmail.com>
2019-11-15 15:44:18 +0100

    State and StateR monad added

--- src/State.ts
+++ src/State.ts
@@ -1,8 +1,89 @@
-import { getApplicative as tupleGetApplicative } from 'fp-ts/lib/Tuple'
-import { Cmd, none, getMonoid } from './Cmd'
+import { Bifunctor2 } from 'fp-ts/lib/Bifunctor'
+import { Semigroupoid2 } from 'fp-ts/lib/Semigroupoid'
+import { Comonad2 } from 'fp-ts/lib/Comonad'
+import { Foldable2 } from 'fp-ts/lib/Foldable'
+import { Traversable2 } from 'fp-ts/lib/Traversable'
+import { Applicative } from 'fp-ts/lib/Applicative'
+import { HKT } from 'fp-ts/lib/HKT'
+import { pipeable } from 'fp-ts/lib/pipeable'
+import { Monad2 } from 'fp-ts/lib/Monad'
+import { Cmd, cmd as cmd_, none, getMonoid } from './Cmd'
+
+declare module 'fp-ts/lib/HKT' {
+  interface URItoKind2<E, A> {
+    'effe-ts/State': State<A, E>
+  }
+}
+
+export const URI = 'effe-ts/State'
+export type URI = typeof URI
 
 export type State<Model, Action = never> = [Model, Cmd<Action>]
 
-export const of = <Model>(model: Model): State<Model, never> => [model, none]
+export const model = <Model, Action>(state: State<Model, Action>): Model => state[0]
+export const cmd = <Model, Action>(state: State<Model, Action>): Cmd<Action> => state[1]
 
-export const getApplicative = <Action>() => tupleGetApplicative(getMonoid<Action>())
+export const of = <Model, Action>(model: Model): State<Model, Action> => [model, none]
+
+const monoidCmd = getMonoid<any>()
+
+export const state: Semigroupoid2<URI> & Bifunctor2<URI> & Comonad2<URI> & Foldable2<URI> & Traversable2<URI> & Monad2<URI> = {
+  URI,
+  compose: (ba, ae) => [model(ba), cmd(ae)],
+  map: (ma, f) => [f(ma[0]), ma[1]],
+  bimap: (ma, f, g) => [g(model(ma)), cmd_.map(cmd(ma), f)],
+  mapLeft: (ma, f) => [model(ma), cmd_.map(cmd(ma), f)],
+  ap: (fab, fa) => [model(fab)(model(fa)), monoidCmd.concat(cmd(fab), cmd(fa))],
+  of,
+  chain: (fa, f) => {
+    const [b, s] = f(model(fa))
+    return [b, monoidCmd.concat(cmd(fa), s)]
+  },
+  extract: model,
+  extend: (ae, f) => [f(ae), cmd(ae)],
+  reduce: (ae, b, f) => f(b, model(ae)),
+  foldMap: _ => (ae, f) => f(model(ae)),
+  reduceRight: (ae, b, f) => f(model(ae), b),
+  traverse: <F>(F: Applicative<F>) => <A, S, B>(as: State<A, S>, f: (a: A) => HKT<F, B>): HKT<F, State<B, S>> => {
+    return F.map(f(model(as)), b => [b, cmd(as)])
+  },
+  sequence: <F>(F: Applicative<F>) => <A, S>(fas: State<HKT<F, A>, S>): HKT<F, State<A, S>> => {
+    return F.map(model(fas), a => [a, cmd(fas)])
+  }
+}
+
+const {
+  ap,
+  apFirst,
+  apSecond,
+  bimap,
+  chain,
+  chainFirst,
+  compose,
+  duplicate,
+  extend,
+  flatten,
+  foldMap,
+  map,
+  mapLeft,
+  reduce,
+  reduceRight
+} = pipeable(state)
+
+export {
+  ap,
+  apFirst,
+  apSecond,
+  bimap,
+  chain,
+  chainFirst,
+  compose,
+  duplicate,
+  extend,
+  flatten,
+  foldMap,
+  map,
+  mapLeft,
+  reduce,
+  reduceRight
+}
--- src/StateR.ts
+++ src/StateR.ts
@@ -1,13 +1,94 @@
-import { getApplicative as tupleGetApplicative } from 'fp-ts/lib/Tuple'
-import { CmdR, none, getMonoid, fromCmd } from './CmdR'
+import { Applicative } from 'fp-ts/lib/Applicative'
+import { HKT } from 'fp-ts/lib/HKT'
+import { pipeable } from 'fp-ts/lib/pipeable'
+import { Semigroupoid3 } from 'fp-ts/lib/Semigroupoid'
+import { Bifunctor3 } from 'fp-ts/lib/Bifunctor'
+import { Comonad3 } from 'fp-ts/lib/Comonad'
+import { Foldable3 } from 'fp-ts/lib/Foldable'
+import { Traversable3 } from 'fp-ts/lib/Traversable'
+import { Monad3 } from 'fp-ts/lib/Monad'
+import { CmdR, none, cmdr as cmdr_, getMonoid, fromCmd } from './CmdR'
 import { State } from './State'
+
+declare module 'fp-ts/lib/HKT' {
+  interface URItoKind3<R, E, A> {
+    'effe-ts/StateR': StateR<R, A, E>
+  }
+}
+
+export const URI = 'effe-ts/StateR'
+export type URI = typeof URI
 
 export type StateR<Env, Model, Action = never> = [Model, CmdR<Env, Action>]
 
-export const of = <Model>(model: Model): StateR<{}, Model, never> => [model, none]
+export const model = <Env, Model, Action>(state: StateR<Env, Model, Action>): Model => state[0]
+export const cmdr = <Env, Model, Action>(state: StateR<Env, Model, Action>): CmdR<Env, Action> => state[1]
 
-export const getApplicative = <Env, Action>() => tupleGetApplicative(getMonoid<Env, Action>())
+export const of = <Env, Model, Action>(model: Model): StateR<Env, Model, Action> => [model, none]
 
 export function fromState<Model, Action>([model, cmd]: State<Model, Action>): StateR<{}, Model, Action> {
   return [model, fromCmd(cmd)]
+}
+
+const monoidCmd = getMonoid<any, any>()
+
+export const stater: Semigroupoid3<URI> & Bifunctor3<URI> & Comonad3<URI> & Foldable3<URI> & Traversable3<URI> & Monad3<URI> = {
+  URI,
+  compose: (ba, ae) => [model(ba), cmdr(ae)],
+  map: (ma, f) => [f(ma[0]), ma[1]],
+  bimap: (ma, f, g) => [g(model(ma)), cmdr_.map(cmdr(ma), f)],
+  mapLeft: (ma, f) => [model(ma), cmdr_.map(cmdr(ma), f)],
+  ap: (fab, fa) => [model(fab)(model(fa)), monoidCmd.concat(cmdr(fab), cmdr(fa))],
+  of,
+  chain: (fa, f) => {
+    const [b, s] = f(model(fa))
+    return [b, monoidCmd.concat(cmdr(fa), s)]
+  },
+  extract: model,
+  extend: (ae, f) => [f(ae), cmdr(ae)],
+  reduce: (ae, b, f) => f(b, model(ae)),
+  foldMap: _ => (ae, f) => f(model(ae)),
+  reduceRight: (ae, b, f) => f(model(ae), b),
+  traverse: <F>(F: Applicative<F>) => <R, A, S, B>(as: StateR<R, A, S>, f: (a: A) => HKT<F, B>): HKT<F, StateR<R, B, S>> => {
+    return F.map(f(model(as)), b => [b, cmdr(as)])
+  },
+  sequence: <F>(F: Applicative<F>) => <R, A, S>(fas: StateR<R, HKT<F, A>, S>): HKT<F, StateR<R, A, S>> => {
+    return F.map(model(fas), a => [a, cmdr(fas)])
+  }
+}
+
+const {
+  ap,
+  apFirst,
+  apSecond,
+  bimap,
+  chain,
+  chainFirst,
+  compose,
+  duplicate,
+  extend,
+  flatten,
+  foldMap,
+  map,
+  mapLeft,
+  reduce,
+  reduceRight
+} = pipeable(stater)
+
+export {
+  ap,
+  apFirst,
+  apSecond,
+  bimap,
+  chain,
+  chainFirst,
+  compose,
+  duplicate,
+  extend,
+  flatten,
+  foldMap,
+  map,
+  mapLeft,
+  reduce,
+  reduceRight
 }
