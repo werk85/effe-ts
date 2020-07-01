2ed48a45073cab7a6b324f31b8db093a12a778aa
Malte Legenhausen <mlegenhausen@gmail.com>
2020-07-01 09:21:13 +0200

    Dependencies updated. Navigation now returns a LocationUpdate. Version bumped to 0.4.0

--- src/Navigation.ts
+++ src/Navigation.ts
@@ -3,7 +3,7 @@ import { pipe } from 'fp-ts/lib/pipeable'
 import * as R from 'fp-ts-rxjs/lib/Observable'
 import * as IO from 'fp-ts/lib/IO'
 import { Reader } from 'fp-ts/lib/Reader'
-import { Location as HistoryLocation, History } from 'history'
+import { Location as HistoryLocation, History, Listener } from 'history'
 import * as RO from 'fp-ts-rxjs/lib/ReaderObservable'
 import * as T from 'fp-ts/lib/Task'
 import * as sub from './Sub'
@@ -13,18 +13,23 @@ import { perform_ } from './Cmd'
 
 export interface Location extends HistoryLocation {}
 
+export interface LocationUpdate {
+  action: 'POP' | 'PUSH' | 'REPLACE'
+  location: Location
+}
+
 export interface NavigationHistory {
   location: Location
   push(url: string): IO.IO<void>
-  goBack: IO.IO<void>
-  listen(f: (location: Location) => void): IO.IO<void>
+  back: IO.IO<void>
+  listen(f: Listener<any>): IO.IO<void>
 }
 
 export function history(history: History): NavigationHistory {
   return {
     location: history.location,
     push: url => () => history.push(url),
-    goBack: () => history.goBack(),
+    back: () => history.back(),
     listen: f => () => history.listen(f)
   }
 }
@@ -37,7 +42,7 @@ export const push = <Env extends NavigationEnv>(url: string): RO.ReaderObservabl
   perform_(env => T.fromIO(env.history.push(url)))
 
 export const goBack = <Env extends NavigationEnv>(): RO.ReaderObservable<Env, never> =>
-  perform_(env => T.fromIO(env.history.goBack))
+  perform_(env => T.fromIO(env.history.back))
 
 export interface Program<Model, Action, DOM> extends html.Program<Model, Action, DOM> {
   listen: IO.IO<void>
@@ -45,7 +50,7 @@ export interface Program<Model, Action, DOM> extends html.Program<Model, Action,
 export interface ReaderProgram<Env, Model, Action, DOM> extends Reader<Env, Program<Model, Action, DOM>> {}
 
 export function program<Env extends NavigationEnv, Model, Action, DOM>(
-  locationToMessage: (location: Location) => Action,
+  locationUpdateToMessage: (update: LocationUpdate) => Action,
   init: (location: Location) => State<Env, Model, Action>,
   update: (action: Action) => (model: Model) => State<Env, Model, Action>,
   view: (model: Model) => html.Html<DOM, Action>,
@@ -54,19 +59,14 @@ export function program<Env extends NavigationEnv, Model, Action, DOM>(
   const monoidSubR = sub.getMonoid<Env, Model, Action>()
 
   return env => {
-    const location$ = new Subject<Location>()
+    const update$ = new Subject<LocationUpdate>()
 
-    const onChangeLocation = sub.fromObservable(
-      pipe(
-        location$,
-        R.map(location => locationToMessage(location))
-      )
-    )
+    const onChangeLocation = sub.fromObservable(pipe(update$, R.map(locationUpdateToMessage)))
     const subs: sub.Sub<Env, Model, Action> = monoidSubR.concat(subscriptions, onChangeLocation)
 
     return {
       ...html.program(init(env.history.location), update, view, subs)(env),
-      listen: env.history.listen(location => location$.next(location))
+      listen: env.history.listen(update => update$.next(update))
     }
   }
 }
